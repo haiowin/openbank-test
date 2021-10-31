c157556a566062751ab29f5046890e35ae2fd508
cmhernandezdel <cmhernandezdel@gmail.com>
2021-10-31 08:50:38 +0100

    Add item click function

--- app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/ListItemViewModel.kt
+++ app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/ListItemViewModel.kt
@@ -3,8 +3,14 @@ package com.cmhernandezdel.openbanktest.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cmhernandezdel.openbanktest.models.MarvelAPIResult
 
-class ListItemViewModel(private val characterName: String): ViewModel() {
-    private val _name = MutableLiveData(characterName)
+class ListItemViewModel(private val character: MarvelAPIResult, private val onItemClicked: (character: MarvelAPIResult) -> Unit):
+    ViewModel() {
+    private val _name = MutableLiveData(character.name)
     val name: LiveData<String> by this::_name
+
+    fun onClick() {
+        this.onItemClicked(this.character)
+    }
 }
--- app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/ListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cmhernandezdel.openbanktest.models.MarvelAPIResult
 import com.cmhernandezdel.openbanktest.providers.ApiProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,10 +13,16 @@ import javax.inject.Inject
 @HiltViewModel
 class ListViewModel @Inject constructor(private val apiProvider: ApiProvider) : ViewModel() {
     private val _characters = MutableLiveData<List<ListItemViewModel>>()
+    private val _selectedCharacter = MutableLiveData<ListItemViewModel>()
     val characters: LiveData<List<ListItemViewModel>> by this::_characters
+    val selectedCharacter: LiveData<ListItemViewModel> by this::_selectedCharacter
 
     init {
         retrieveCharacters()
+    }
+
+    private fun onItemClicked(character: MarvelAPIResult) {
+
     }
 
     private fun retrieveCharacters() = viewModelScope.launch {
@@ -23,10 +30,9 @@ class ListViewModel @Inject constructor(private val apiProvider: ApiProvider) : 
         response?.let {
             val characterList = ArrayList<ListItemViewModel>()
             for (character in it.data.results) {
-                val viewModel = ListItemViewModel(character.name)
+                val viewModel = ListItemViewModel(character, ::onItemClicked)
                 characterList.add(viewModel)
             }
-            println("Post list of ${characterList.size} characters")
             _characters.postValue(characterList)
         }
     }
