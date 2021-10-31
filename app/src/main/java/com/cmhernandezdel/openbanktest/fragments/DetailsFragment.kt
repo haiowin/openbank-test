115e0c16609f10e3d18208c5b6eacbfadf04b9e3
cmhernandezdel <cmhernandezdel@gmail.com>
2021-10-31 09:30:34 +0100

    Fix binding

--- app/src/main/java/com/cmhernandezdel/openbanktest/fragments/DetailsFragment.kt
+++ app/src/main/java/com/cmhernandezdel/openbanktest/fragments/DetailsFragment.kt
@@ -3,16 +3,32 @@ package com.cmhernandezdel.openbanktest.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.cmhernandezdel.openbanktest.R
 import com.cmhernandezdel.openbanktest.viewmodels.DetailsViewModel
+import com.cmhernandezdel.openbanktest.viewmodels.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import com.cmhernandezdel.openbanktest.databinding.FragmentDetailsBinding
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
     private val mViewModel : DetailsViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailsBinding.bind(view)
+        binding.apply {
+            viewModel = mViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        mSharedViewModel.currentCharacter.observe(viewLifecycleOwner, {
+            if(it != null) mViewModel.setDisplayCharacter(it)
+            else mViewModel.unsetDisplayCharacter()
+        })
+
     }
 }
--- app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/cmhernandezdel/openbanktest/viewmodels/DetailsViewModel.kt
@@ -3,12 +3,22 @@ package com.cmhernandezdel.openbanktest.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cmhernandezdel.openbanktest.models.MarvelAPIResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailsViewModel @Inject constructor() : ViewModel() {
-    private val _displayCharacter = MutableLiveData<MarvelAPIResult>()
-    val displayCharacter : LiveData<MarvelAPIResult> by this::_displayCharacter
+    private val _displayCharacter = MutableLiveData<MarvelAPIResult?>()
+    val displayCharacter: LiveData<MarvelAPIResult?> by this::_displayCharacter
+
+    fun setDisplayCharacter(character: MarvelAPIResult) = viewModelScope.launch {
+        _displayCharacter.postValue(character)
+    }
+
+    fun unsetDisplayCharacter() = viewModelScope.launch {
+        _displayCharacter.postValue(null)
+    }
 }
